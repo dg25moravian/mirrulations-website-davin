17e7ddc15400d7ce62d7b189cbc2583b77abbc28
Davin Glynn <glynnd@moravian.edu>
2025-04-10 14:29:48 -0400

    pagination

--- javascript/components/pageSwitcher.jsx
+++ javascript/components/pageSwitcher.jsx
@@ -21,16 +21,16 @@ const PageSwitcher = ({ current_page, total_pages, onPageChange }) => {
     pageNumbers.push(i);
   }
 
-  const currentPageNum = Number(current_page);
-  const totalPagesNum = Number(total_pages);
-  
+    const currentPageNum = Number(current_page);
+    const totalPagesNum = Number(total_pages);
+
   const arrowButtons = [
-    { text: "<<", page: 0, disabled: currentPageNum <= 0 },
-    { text: "<", page: currentPageNum - 1, disabled: currentPageNum <= 0 },
-    { text: ">", page: currentPageNum + 1, disabled: currentPageNum >= totalPagesNum - 1 },
-    { text: ">>", page: totalPagesNum - 1, disabled: currentPageNum >= totalPagesNum - 1 },
+    { text: "<<", page: 1, disabled: currentPageNum === 0 },
+    { text: "<", page: Number(current_page) - 1, disabled: currentPageNum === 0 },
+    { text: ">", page: Number(current_page) + 1, disabled: currentPageNum + 1 ===  totalPagesNum},
+    { text: ">>", page: Number(total_pages), disabled: currentPageNum + 1 ===  totalPagesNum},
   ];
-  
+
   return (
     <section id="page_switcher_section" className="container mt-4">
       <div id="page_switcher_container" className="container">
